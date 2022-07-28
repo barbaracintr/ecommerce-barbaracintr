fe924ce8bed16d0050211daabb730b318d4c160a
Bárbara Cintra <b.mcgomes@hotmail.com>
2022-07-28 13:02:35 -0400

    Products in store

---
+++ src/providers/products.js
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+
+import { api } from "../data";
+
+export const ProductsContext = createContext([]);
+
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () => {
+    api
+      .get("/products")
+      .then((response) => setProducts(response.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <ProductsContext.Provider value={{ products, apiProducts: getProducts }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
